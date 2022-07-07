236d5a1f6e9e614d8e58815900ff1cfaf66ae696
thomasaturner <104743671+thomasaturner@users.noreply.github.com>
2022-07-06 22:07:54 -0600

    update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -12,14 +12,14 @@ apikey = os.environ['apikey']
 url = os.environ['url']
 
 
-authenticator = IAMAuthenticator('apikey')
+authenticator = IAMAuthenticator('Cn6BpxJseLB7WhwhmGcilPLB_oFhX_0YslJgDm4oYH7I')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
 language_translator.set_service_url(
-    'url'
+    'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/43a69715-42ae-44a4-ab3d-0b61b916d98b'
 )
 
 
@@ -29,7 +29,7 @@ def english_to_french(english_text):
         text = english_text,
         model_id='en-fr').get_result()
 
-    return french_text.get('translations')[0].get('translate')
+    return french_text.get('translations')[0].get('translation')
 
 
 def french_to_english(french_text):
@@ -37,4 +37,5 @@ def french_to_english(french_text):
     english_text = language_translator.translate(
         text = french_text,
         model_id='fr-en').get_result()
-    return english_text.get('translations')[0].get('translate')
+    return english_text.get('translations')[0].get('translation')
+
